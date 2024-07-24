f7025aa389351114d1c7dce4f42dc13064dd02a1
Ravi <kant.ravi747@gmail.com>
2024-07-25 00:49:01 +0530

    progress of Login page

---
+++ src/screen/HomeScreen.jsx
@@ -0,0 +1,95 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { Suspense } from 'react'
+import { colors } from '../utils/colors'
+import { fonts } from '../utils/fonts'
+import { useNavigation } from '@react-navigation/native'
+
+const HomeScreen = () => {
+  const navigation= useNavigation();
+  const handleLogin= ()=>{
+    navigation.navigate("LOGIN")
+  }
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/logo.png")} style={styles.logo} />
+      <Image source={require("../assets/frame.png")} style={styles.bannerImage} />
+      <Text style={styles.title}>Welcome to the App</Text>
+      <Text style={styles.subtitle}>This app is used for login and signup</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style= {[styles.lognButtonWrapper,
+           {backgroundColor: colors.primary}]}
+           onPress={handleLogin}
+           >
+          <Text style={styles.lognButtonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.lognButtonWrapper,
+          
+        ]}>
+          <Text style={styles.signButtonText}>Sign-Up</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    alignItems: "center"
+  },
+  logo: {
+    height: 50,
+    width: 140,
+    marginVertical: 30
+  },
+  bannerImage: {
+    height: 250,
+    width: 231,
+    marginVertical: 20
+  },
+  title: {
+    fontSize: 36,
+    fontFamily: fonts.Bold,
+    paddingHorizontal: 20,
+    textAlign: "center",
+    color: colors.primary,
+    marginTop: 40
+  },
+  subtitle: {
+    fontSize: 18,
+    fontFamily: fonts.Medium,
+    paddingHorizontal: 20,
+    textAlign: "center",
+    color: colors.secondary,
+    marginVertical: 50
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 60,
+    borderWidth: 2,
+    borderColor: colors.primary,
+    width: "80%",
+    height: 60,
+    borderRadius: 100
+  },
+  lognButtonWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "50%",
+    borderRadius: 90
+  },
+  lognButtonText: {
+    color: colors.white,
+    fontSize: 18,
+    fontFamily: fonts.SemiBold
+  },
+  signButtonText:{
+    color: colors.white,
+    fontSize: 18,
+    fontFamily: fonts.SemiBold
+  }
+
+});
---
+++ src/screen/LoginScreen.jsx
@@ -0,0 +1,97 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import Ionicons from "react-native-vector-icons/Ionicons"
+import SimpleLineIcons from "react-native-vector-icons/SimpleLineIcons"
+import { colors } from '../utils/colors'
+import { fonts } from '../utils/fonts'
+
+const LoginScreen = () => {
+    const [secureEntry, setSecureEntry]= useState(true)
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.backButtonWrapper}>
+                <Ionicons name={"arrow-back-outline"}
+                    color={colors.primary}
+                    size={25} />
+            </TouchableOpacity>
+            <View style={styles.textContainer}>
+                <Text style={styles.headingText}>Hey</Text>
+                <Text style={styles.headingText}>Welcome</Text>
+                <Text style={styles.headingText}>Back</Text>
+            </View>
+            <View style={styles.formContainer}>
+                <View style={styles.inputContainer}>
+                    <Ionicons name={"mail-outline"} size={30}
+                        color={colors.secondary} />
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Enter your email"
+                        placeholderTextColor={colors.secondary}
+                        keyboardType="email-address"
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <SimpleLineIcons name={"lock"} size={30}
+                        color={colors.secondary} />
+                    <TextInput style={styles.textInput}
+                        placeholder="Enter your password" 
+                        placeholderTextColor={colors.secondary} 
+                        secureTextEntry= {secureEntry}
+                        />
+                        <TouchableOpacity onPress={()=>{
+                            setSecureEntry((prev)=>!prev)
+                        }}>
+                        <SimpleLineIcons name={"eye"} size={20}
+                        color={colors.secondary} />
+                        
+                        </TouchableOpacity>
+                </View>
+                
+            </View>
+        </View>
+    );
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.white,
+        padding: 20
+    },
+    backButtonWrapper: {
+        height: 40,
+        width: 40,
+        backgroundColor: colors.gray,
+        borderRadius: 20,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    textContainer: {
+        marginVertical: 20,
+    },
+    headingText: {
+        fontSize: 32,
+        color: colors.primary,
+        fontFamily: fonts.SemiBold,
+    },
+    formContainer: {
+        marginTop: 20,
+    },
+    inputContainer: {
+        borderWidth: 1,
+        borderColor: colors.secondary,
+        borderRadius: 100,
+        paddingHorizontal: 20,
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 2,
+        marginVertical:10
+    },
+    textInput: {
+        flex: 1,
+        paddingHorizontal: 10,
+        fontFamily: fonts.Light,
+    }
+});
